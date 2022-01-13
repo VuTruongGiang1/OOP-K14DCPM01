af2e1479efee550ccc2a6c33e12ef783c65f236b
VuTruongGiang1 <vugiang4335@gmail.com>
2022-01-13 14:35:36 +0700

    UP

---
+++ src/oop/AccountTestDrive.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class AccountTestDrive {
+    public static void main(String[] args) {
+        //Bien tham chieu doi tuong
+        Account account;
+
+        account = new Account();
+
+        account.show();
+        account.depoist();
+        account.show();
+
+        account.withdraw();
+        account.show();
+    }
+    
+}
