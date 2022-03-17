bac7595dd0024654e786336b1384045aa0f90cc1
VuTruongGiang1 <vugiang4335@gmail.com>
2022-03-17 16:01:52 +0700

    kiem tra giua ky

---
+++ src/kiemtragiuaky/GiaoDichNhaDat.java
@@ -0,0 +1,12 @@
+package kiemtragiuaky;
+
+import java.sql.Date;
+
+public class GiaoDichNhaDat {
+    private String maGiaDich;
+    private Date ngayGiaoDich;
+    private int donGia;
+    private int loai;
+    private Double dienTich;
+    
+}
